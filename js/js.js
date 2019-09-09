5dc6c2c383f1bfc66fd43b0ec0af1d6c2cb1cc58
ScarfoneEmi <54369198+ScarfoneEmi@users.noreply.github.com>
2019-09-09 11:25:27 -0300

    nav expand FIX

--- js/js.js
+++ js/js.js
@@ -1,4 +1,16 @@
  $(document).ready(function(){
-alert();
+   $(".nav-item").addClass("sumir");
+});
+var cont=0;
+function recolher(){
 
- });
+if(cont % 2 == 0){
+  $(".nav-item").removeClass("sumir");
+cont++;
+}
+else {
+  $(".nav-item").addClass("sumir");
+  cont++;
+
+}
+}
